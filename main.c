91ba2759b48e0185f5606bfdddef91d048ef313f
Eetu <99178022+E3TU@users.noreply.github.com>
2025-03-05 21:03:08 +0200

    Added ncurses

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <ncurses.h>
 
 int main(void)
 {
+    initscr();
+    noecho();
+    curs_set(0);
+
     time_t t;
     struct tm *tm_info;
 
@@ -11,11 +16,13 @@ int main(void)
         time(&t);
         tm_info = localtime(&t);
 
-        printf("%02d:%02d:%02d\n", tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec);
+        // printf("%02d:%02d:%02d\n", tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec);
+        clear();
+        mvprintw(LINES / 2, (COLS - 8) / 2, "%02d:%02d:%02d", tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec);
+        refresh();
 
-        fflush(stdout);
         sleep(1);
     }
-
+    endwin();
     return 0;
 }
